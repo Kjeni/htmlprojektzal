9a45d695edf5ceec400d205ad9df3054927db700
Kjeni <kacper.blazejewski88@gmail.com>
2023-01-03 20:57:22 +0100

    działa jako tako
    zwraca wartosc ale jest ona nie zaokraglona

--- kalkulator kalorii/script.js
+++ kalkulator kalorii/script.js
@@ -1,15 +1,13 @@
 let isCalculating = false;
-const button=document.querySelector('#button');
+const button = document.querySelector('#button');
 
 function calculateBMR(weight, height, age, gender) {
-    if (gender === 'male') {
-      return 66 + (13.7 * weight) + (5 * height) - (6.8 * age);
-    } else if (gender === 'female') {
-      return 655 + (9.6 * weight) + (1.8 * height) - (4.7 * age);
-    }
-  }
-
-  function calculateCaloricIntake(activityLevel, BMR) {
+  if (gender === 'male') {
+    return 66 + (13.7 * weight) + (5 * height) - (6.8 * age);
+} else if (gender === 'female') {
+    return 655 + (9.6 * weight) + (1.8 * height) - (4.7 * age);
+}}
+function calculateCaloricIntake(activityLevel, BMR) {
     if (activityLevel === 'sedentary') {
       return BMR * 1.2;
     } else if (activityLevel === 'lightly active') {
@@ -22,20 +20,25 @@ function calculateBMR(weight, height, age, gender) {
       return BMR * 1.9;
     }
   }
-  button.addEventListener("click",function calculateCaloricIntake() {
-    
+  
+  const calculateCaloricIntakeFunction = function() {
     if (isCalculating) return;
     isCalculating = true;
-
+  
     const weight = document.getElementById('weight').value;
     const height = document.getElementById('height').value;
     const age = document.getElementById('age').value;
     const gender = document.querySelector('input[name="gender"]:checked').value;
     const activityLevel = document.getElementById('activity-level').value;
-
+  
     const BMR = calculateBMR(weight, height, age, gender);
     const caloricIntake = calculateCaloricIntake(activityLevel, BMR);
     document.getElementById('result').innerHTML = `Your caloric intake should be ${caloricIntake} calories.`;
-}
-)
+  };
+  
+button.addEventListener('click', calculateCaloricIntakeFunction);
+const resetButton = document.getElementById('reset');
 
+resetButton.addEventListener('click', function() {
+    document.getElementById('form').reset();
+});
